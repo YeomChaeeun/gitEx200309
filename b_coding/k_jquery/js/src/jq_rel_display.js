f04974bc965731b031842c0d7b9fe9f48871696b
YeomChaeEun <yce0222@gmail.com>
2020-05-11 17:56:44 +0900

    menu, tab, accodion menu 만들기

--- b_coding/k_jquery/js/src/jq_rel_display.js
+++ b_coding/k_jquery/js/src/jq_rel_display.js
@@ -50,25 +50,103 @@
     testDt.eq(i).next('dd').stop().slideDown();
   });
   // -----------------------------------------------------------------
-
-  var accMenu = $('.accoordion_menu');
+  
+  var accMenu = $('.accordion_menu');
   var accSt01 = accMenu.children('.acc_style_01');
   var accSt01_dt = accSt01.find('dt');
   var accSt01_dd = accSt01.find('dd');
-
-  // console.log(accSt01_dt.length,accSt01_dd.length);
-
-  // dt클릭시 바로 뒤에있는 dd를 나타나게 해라
-  accSt01_dt.on('click',function(){
-    //바로 뒤에있는 dd('accSt01_dd')를 나타나게 해라
-    var thisView = $(this).next('dd').css('display');
-    if( thisView ==='none'){
-      accSt01_dd.slideUp();
+  
+  // console.log( accSt01_dt.length, accSt01_dd.length );
+  
+  // dt('accSt01_dt')를 클릭시(버튼을 클릭하는 것으로 설정/focus처리)
+  accSt01_dt.find('button').on('click', function(e){
+    e.preventDefault();
+    var dtPar = $(this).parent('dt');
+    var thisPar = dtPar.next('dd');
+    var thisView =  thisPar.css('display') === 'none';
+    if(thisView){
+      accSt01_dd.stop().slideUp();
+      thisPar.stop().slideDown();
+      dtPar.addClass('action');
+      dtPar.siblings('dt').removeClass('action');
+    }else{
+      accSt01_dd.stop().slideUp();
+      dtPar.removeClass('action');
     }
-    $(this).next('dd').slideDown();
+    
+  }); 
+  
+  /**
+   * 몇가지 문제점 정리 ...
+   * 1. slide 처리시 toggle이 중복현상
+   * 2. focus 처리시 색상처리
+   * 3. 변수 정리
+   * 
+   */
+  
+  // -----------------------------------------------------------------
+  
+  var tab02 = $('.tabtab');
+  var tabSt02 = tab02.children('.style_02');
+  var tabSt02_dt = tabSt02.find('dt');
+  var tabSt02_dd = tabSt02.find('dd');
+  
+  tabSt02_dd.eq(0).show();
+  tabSt02_dt.find('button').on('click', function(e){
+    e.preventDefault();
+    
+    var dtPar = $(this).parent('dt');
+    var thisPar = dtPar.next('dd');
+    var thisView =  thisPar.css('display') === 'none';
+    
+    if(thisView){
+      tabSt02_dd.hide();
+      thisPar.stop().show();
+    }else{
+    }
   });
+  // -----------------------------------------------------------------
+  // 아코디언 메뉴 html 구조를 이용한 탭메뉴 만들기
   
+  var moreBtn = $('.more_style').find('dt').children('button');
   
+  moreBtn.on('click', function(e){
+    e.preventDefault();
+    
+    var thisDt = $(this).parent('dt');
+    var dtNextView = $(this).parent('dt').next('dd').css('display')==='none';
+    
+    if(dtNextView){
+      $('.more_style').find('dd').hide();
+      thisDt.next('dd').fadeIn();
+      
+      thisDt.siblings('dt').removeClass('action');
+      thisDt.addClass('action');
+    }
+  });
+  // -----------------------------------------------------------------
+
+  var titleBtn = $('.menu_style').find('dt').children('button');
+
+  titleBtn.on('click',function(e){
+    e.preventDefault();
+
+    var thisDt = $(this).parent('dt');
+    var dtNextView = thisDt.next('dd').css('display')==='none';
+
+    if(dtNextView){
+      $('.menu_style').find('dd').slideUp(200);
+      thisDt.next('dd').slideDown(200);
+      thisDt.siblings('dt').removeClass('action');
+      thisDt.addClass('action');
+    }else{
+      thisDt.next('dd').slideUp(200);
+      thisDt.removeClass('action');
+    }
+  });
+
+
+
 
   //jquery end =============================================================
 })(jQuery);
