525c10de4882ba910ab8ee9d540d6de7803d8614
YeomChaeEun <yce0222@gmail.com>
2020-06-03 13:17:45 +0900

    parallax 2번째 박스 p태그 처리

--- b_coding/k_jquery/js/src/parallax.js
+++ b_coding/k_jquery/js/src/parallax.js
@@ -51,18 +51,22 @@
     if(winTop2>imgArr[0] && winTop2<=imgArr[1]){
       console.log('!!!!!!! ------------------------1');
       divImg.eq(0).css({backgroundPositionY: -(imgArr[0]-winTop2)/20+'px'});
+      divImg.eq(0).next('p').css({top:-(imgArr[0]-winTop2)/5});
     }
     if(winTop2>imgArr[1] && winTop2<=imgArr[2]){
       console.log('!!!!!!! ------------------------2');
       divImg.eq(1).css({backgroundPositionY: -(imgArr[1]-winTop2)/20+'px'});
+      divImg.eq(1).next('p').css({left:-(imgArr[1]-winTop2)/5});
     }
     if(winTop2>imgArr[2] && winTop2<=imgArr[3]){
       console.log('!!!!!!! ------------------------3');
       divImg.eq(2).css({backgroundPositionY: -(imgArr[2]-winTop2)/20+'px'});
+      divImg.eq(2).next('p').css({right:(imgArr[2]-winTop2)/5});
     }
     if(winTop2>imgArr[3]){
       console.log('!!!!!!! ------------------------4');
       divImg.eq(2).css({backgroundPositionY: -(imgArr[3]-winTop2)/20+'px'});
+      divImg.eq(3).next('p').css({top:-(imgArr[3]-winTop2)/25});
     }
   });
 
