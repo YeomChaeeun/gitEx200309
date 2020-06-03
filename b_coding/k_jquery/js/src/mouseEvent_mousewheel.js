24e5d016eddcf83b48c3e0d00fd65f0c2249e7fd
YeomChaeEun <yce0222@gmail.com>
2020-06-03 17:13:26 +0900

    mouseEvent mousewheel 동작 처리 가로스크롤

--- b_coding/k_jquery/js/src/mouseEvent_mousewheel.js
+++ b_coding/k_jquery/js/src/mouseEvent_mousewheel.js
@@ -5,9 +5,17 @@
   var colorArr = ['#fa0','#acf','#cfa','#5df','#fac'];
   var n = 0;
 
-  var mv = $('.mv');
+  var mw = $('.mw');
+  mw.css({overflow:'hidden'});
+
+  var mwUl = mw.find('ul');
+  var mwLi = mwUl.find('li');
+  for(var i=0;i<mwLi.length;i++){
+    mwLi.eq(i).css({backgroundColor:colorArr[i]});
+  }
+
   var mouseOk = true;
-  mv.on('mousewheel DOMMouseScroll',function(e){
+  mw.on('mousewheel DOMMouseScroll',function(e){
     // console.log(e.type);
     // console.log(e.originalEvent.wheelDelta);
     // console.log(e.originalEvent.detail);
@@ -16,9 +24,9 @@
       var evt = e.originalEvent;
       var mouseResult;
       if(evt.wheelDelta){
-        mouseResult = evt.wheelDelta;
+        mouseResult = -evt.wheelDelta;
       }else{
-        mouseResult = evt.detail*-40;
+        mouseResult = evt.detail*40;
       }
     }
     console.log(mouseResult);
@@ -26,12 +34,19 @@
 
     if(mouseResult<0 && n>0){
       n-=1;
-    }else if(mouseResult>0 && n<colorArr.length){
+    }else if(mouseResult>0 && n<colorArr.length-1){
       n+=1;
     }
-    mv.stop().animate({backgroundColor:colorArr[n]},300,function(){
-      mouseOk = true;
+    // mw.text(colorArr[n]);
+    // mw.stop().animate({backgroundColor:colorArr[n]},300,function(){
+    //   mouseOk = true;
+    // });
+    // stop([clearQueue],[jumpToEnd]) - 기본값:(false,false)
+    mwUl.stop(false,false).animate({marginLeft:-n*100+'%'},600,function(){
+      console.log(n);
+      mouseOk=true;
     });
+    
   });
 
 
